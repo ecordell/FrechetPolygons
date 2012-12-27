bc75b2db79467b0c3a26f21d79e031f8fb46a038
ecordell <cordell.evan@gmail.com>
2012-12-26 19:54:56 -0600

    Shortest Path implementation, better drawing/data storage

--- src/PolygonInputFrame.java
+++ src/PolygonInputFrame.java
@@ -1,37 +1,40 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.geom.Path2D;
+import java.awt.geom.PathIterator;
+import java.awt.geom.Point2D;
 
 public class PolygonInputFrame extends JFrame implements ActionListener {
 	PlotArea drawPanel;
 	ControlArea buttonPanel;
-	Polygon polyP;
-	Polygon polyQ;
+	Point2D.Double[] polyP;
+	Point2D.Double[] polyQ;
+	
 	public PolygonInputFrame() {
     	super("Frechet Distance - Simple Polygons");
     	drawPanel = new PlotArea();
     	this.getContentPane().add(drawPanel);
     	this.addTestPolygons();
-    }
+    	ShortestPath spCalculator = new ShortestPath(polyQ, polyQ[0], polyQ[2]);
+    	drawPanel.spPath = spCalculator.getPath();
+	}
 	
 	private void addTestPolygons() {
-		polyP = new Polygon();
-		polyP.addPoint(drawPanel.toScreenX(-1), drawPanel.toScreenY(-1));
-    	polyP.addPoint(drawPanel.toScreenX(0), drawPanel.toScreenY(1));
-    	polyP.addPoint(drawPanel.toScreenX(1), drawPanel.toScreenY(-1));
-    	polyP.addPoint(drawPanel.toScreenX(0), drawPanel.toScreenY(0.6));
-    	
-    	drawPanel.polyP = polyP;
-    	
-    	
-    	
-    	polyQ = new Polygon();
-    	
-    	polyQ.addPoint(drawPanel.toScreenX(-1), drawPanel.toScreenY(1));
-    	polyQ.addPoint(drawPanel.toScreenX(0),  drawPanel.toScreenY(-1));
-    	polyQ.addPoint(drawPanel.toScreenX(1), drawPanel.toScreenY(1));
-    	polyQ.addPoint(drawPanel.toScreenX(0), drawPanel.toScreenY(-0.6));
-    	
+		polyP = new Point2D.Double[4];
+		polyP[0] = new Point2D.Double(-1, -1);
+		polyP[1] = new Point2D.Double(0, 1);
+		polyP[2] = new Point2D.Double(1, -1);
+		polyP[3] = new Point2D.Double(0, 0.6);
+		
+		drawPanel.polyP = polyP;
+		
+		polyQ = new Point2D.Double[4];
+		polyQ[0] = new Point2D.Double(-1, 1);
+		polyQ[1] = new Point2D.Double(0, -1);
+		polyQ[2] = new Point2D.Double(1, 1);
+		polyQ[3] = new Point2D.Double(0, -0.6);
+ 
     	drawPanel.polyQ = polyQ;
 	}
 	
@@ -61,8 +64,9 @@ class ControlArea extends JPanel{
 }
 
 class PlotArea extends JPanel{
-	public Polygon polyP;
-	public Polygon polyQ;
+	public Point2D.Double[] polyP;
+	public Point2D.Double[] polyQ;
+	public Point2D.Double[] spPath;
 	private double xmin = -2;
 	private double xmax = 2;
 	private double ymin = -2;
@@ -74,11 +78,36 @@ class PlotArea extends JPanel{
 	public void paint(Graphics g)
 	{	
 		g.setColor(Color.blue);
-		g.drawPolygon(polyP);
+		drawPolygon(polyP, g);
 		g.setColor(Color.red);
-		g.drawPolygon(polyQ);
+		drawPolygon(polyQ, g);
+		if (spPath != null) {
+			drawPath(spPath, g);
+		}
     	super.paintComponents(g);
 	}
+	
+	public void drawPolygon (Point2D.Double[] points, Graphics g) {
+		Point[] screenPoints = new Point[points.length];
+		for (int i = 0; i<points.length; i++) {
+			screenPoints[i] = new Point(toScreenX(points[i].x), toScreenY(points[i].y));		
+		}
+		for (int i = 0; i<screenPoints.length; i++) {
+			int j = i + 1;
+			if (i == screenPoints.length - 1) {
+				j = 0;
+			}
+			g.drawLine(screenPoints[i].x, screenPoints[i].y, screenPoints[j].x, screenPoints[j].y);
+		}
+	}
+	
+	public void drawPath(Point2D.Double[] points, Graphics g) {
+		g.setColor(Color.green);
+		for (int i = 0; i< points.length - 1; i++) {
+			g.drawLine(toScreenX(points[i].x), toScreenY(points[i].y), toScreenX(points[i+1].x), toScreenY(points[i+1].y));
+		}
+	}
+	
 	public double toMathX(int x)
 	{ 
 	    return (x - (this.getWidth()) / 2) * ((xmax - xmin) / (this.getWidth())) + ((xmax + xmin) / 2);
---
+++ src/ShortestPath.java
@@ -0,0 +1,207 @@
+import java.awt.Point;
+import java.awt.Polygon;
+import java.awt.geom.GeneralPath;
+import java.awt.geom.Point2D;
+import java.util.Arrays;
+
+//Always world coordinates in this class
+
+class SPTreeNode {
+	public double x;
+	public double y;
+	public double totalDist;
+	public int prev;
+}
+
+public class ShortestPath {
+	Point2D.Double[] _poly;
+	Point2D.Double _start;
+	Point2D.Double _end;
+	Point2D.Double _path[];
+	
+	public ShortestPath(Point2D.Double[] poly, Point2D.Double start, Point2D.Double end) {
+    	this._poly = poly;
+    	this._start = start;
+    	this._end = end;
+    }
+	
+	public Point2D.Double[] getPath() {
+		this.shortestPath(_start.x, _start.y, _end.x, _end.y);
+		System.out.println(Arrays.toString(_path));
+		return _path;
+	}
+	
+	public double calcDist(double startX, double startY, double endX, double endY) {
+		double dX = endX - startX;
+		double dY = endY - startY;
+		return Math.sqrt(dX * dX + dY*dY);
+	}
+	
+	private double getZero(double x) {
+		double testPositiveZero = 0.00000000001;
+		double testNegativeZero = -0.00000000001;
+		if (x >= testNegativeZero && x <= testPositiveZero) {
+			x = 0;
+		}
+		return x;
+	}
+	
+	 //http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html
+	 private boolean pointInPolygon(double testX, double testY) {
+		 int i, j = 0;
+		 boolean odd = false;
+		  for (i = 0, j = _poly.length-1; i < _poly.length; j = i++) {
+		    if ( ((_poly[i].y>testY) != (_poly[j].y>testY)) &&
+			 (testX < (_poly[j].x-_poly[i].x) * (testY-_poly[i].y) / (_poly[j].y-_poly[i].y) + _poly[i].x) )
+		       odd = !odd;
+		  }
+		  return odd;
+	}
+	
+	private boolean lineInPolygon(double testSX, double testSY, double testEX, double testEY) {
+		double theCos, theSin, dist, sX, sY, eX, eY, rotSX, rotSY, rotEX, rotEY, crossX;
+		
+		testEX = testEX - testSX;
+		testEY = testEY - testSY;
+		dist = Math.sqrt( testEX * testEX + testEY * testEY );
+		theCos = testEX / dist;
+		theSin = testEY / dist;
+
+	    for (int i = 0; i< _poly.length; i++) {
+	        int j = i + 1;
+	        if (j >= _poly.length) {
+	                j = 1;
+	        }
+
+	        sX = _poly[i].x - testSX;
+	        sY = _poly[i].y - testSY;
+	        eX = _poly[j].x - testSX;
+	        eY = _poly[j].y - testSY;
+
+	        if ((sX == 0 && sY == 0 && eX == testEX && eY == testEY) || (eX == 0 && eY == 0 && sX == testEX && sY == testEY)) {
+	                return true;
+	        }
+
+	        rotSX = getZero(sX * theCos + sY * theSin);
+	        rotSY = getZero(sY * theCos - sX * theSin);
+	        rotEX = getZero(eX * theCos + eY * theSin);
+	        rotEY = getZero(eY * theCos - eX * theSin);
+	        crossX = getZero(rotSX + (rotEX-rotSX)*(0-rotSY)/(rotEY-rotSY));
+
+	        if ((rotSY < 0 && rotEY > 0) || (rotEY < 0 && rotSY > 0)) {
+	                if (crossX >= 0 && crossX <= dist) {
+	                        return false;
+	                }
+	        }
+
+	        if (rotSY == 0 && rotEY == 0 && (rotSX >= 0 || rotEX >= 0) && (rotSX <= dist || rotEX <= dist)
+	                && (rotSX < 0 || rotEX < 0 || rotSX > dist || rotEX > dist)) {
+	                return false;
+	        }
+	   }
+	    
+	   return pointInPolygon(testSX + testEX / 2, testSY + testEY / 2);
+	   
+	}
+	
+	private boolean shortestPath(double sX, double sY, double eX, double eY) {
+		int solutionNodes = 0;
+		
+	  double  INF = Double.POSITIVE_INFINITY;     //  (larger than total solution dist could ever be)
+	
+	  SPTreeNode  pointList[] = new SPTreeNode[_poly.length + 2];   //  (enough for all polycorners plus two)
+	  for(int i = 0; i< _poly.length+2; i++) {
+		pointList[i] = new SPTreeNode();  
+	  }
+	  
+	  int    pointCount;
+	
+	  int     treeCount, i, j, bestI, bestJ;
+	  double  bestDist, newDist ;
+	
+	  //  Fail if either the startpoint or endpoint is outside the polygon set.
+	  //TODO: needs to be tolerant of edges
+	  /*if (!_poly.contains(sX, sY) || !_poly.contains(eX, eY)) {
+	        return false;
+	  }*/
+	
+	  //  If there is a straight-line solution, return with it immediately.
+	  if (lineInPolygon(sX,sY,eX,eY)) {
+	    solutionNodes=0; 
+	    return true; 
+	  }
+	
+	  //  Build a point list that refers to the corners of the
+	  //  polygons, as well as to the startpoint and endpoint.
+	  pointList[0].x=sX;
+	  pointList[0].y=sY; 
+	  pointCount=1;
+	    for (i=0; i<_poly.length; i++) {
+	      pointList[pointCount].x=_poly[i].x;
+	      pointList[pointCount].y=_poly[i].y; 
+	      pointCount++; 
+	      }
+	  pointList[pointCount].x=eX;
+	  pointList[pointCount].y=eY; 
+	  pointCount++;
+	
+	  //  Initialize the shortest-path tree to include just the startpoint.
+	  treeCount=1; 
+	  pointList[0].totalDist = 0.;
+	  bestJ = 1;
+	  bestI = 0;
+	
+	  //  Iteratively grow the shortest-path tree until it reaches the endpoint
+	  //  -- or until it becomes unable to grow, in which case exit with failure.
+	  while (bestJ < pointCount-1) {
+	    bestDist=INF;
+	    for (i=0; i<treeCount; i++) {
+	      for (j=treeCount; j<pointCount; j++) {
+	        if (lineInPolygon(pointList[i].x,pointList[i].y,pointList[j].x,pointList[j].y)) {
+	          newDist=pointList[i].totalDist + calcDist(pointList[i].x,pointList[i].y, pointList[j].x,pointList[j].y);
+	          if (newDist<bestDist) {
+	            bestDist=newDist; 
+	            bestI=i; 
+	            bestJ=j; 
+	            }
+	          }
+	        }
+	      }
+	    if (bestDist==INF) {
+	    	return false;   //  (no solution)
+	    }
+	    pointList[bestJ].prev = bestI;
+	    pointList[bestJ].totalDist = bestDist;
+	    
+	    SPTreeNode tmp = pointList[bestJ];
+	    pointList[bestJ] = pointList[treeCount];
+	    pointList[treeCount] = tmp;
+	            
+	    treeCount++; 
+	   }
+	
+	  //  Load the solution arrays.
+	  solutionNodes = -1; 
+	  i=treeCount-1;
+	  while (i> 0) {
+	    i=pointList[i].prev;
+	    solutionNodes++; 
+	  }
+	  _path = new Point2D.Double[solutionNodes + 2];
+	  _path[0] = new Point2D.Double(sX, sY);
+	  _path[solutionNodes + 1] = new Point2D.Double(eX, eY);
+	  j=solutionNodes-1; 
+	  i=treeCount-1;
+	  while (j>=0) {
+	    i=pointList[i].prev;
+	   
+	    _path[j+1] = new Point2D.Double(pointList[i].x, pointList[i].y);
+	    
+	    j--; 
+	   }
+	  
+	  //  Success.
+	  return true; 
+	}
+	
+}
